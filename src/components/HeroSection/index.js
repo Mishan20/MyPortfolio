a8f07170d808dfef092c5769f42f5fe02608cffa
Mishan20 <ishansenanayaka1@gmail.com>
2023-12-07 15:27:29 +0530

    modified images

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import MyImg from '../../images/m2.JPG'
+import MyImg from '../../images/m3.png'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
