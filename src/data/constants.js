ee215c5aa1475e9181d6d37452d0aec26d220b9b
Mishan20 <ishansenanayaka1@gmail.com>
2023-12-10 16:05:05 +0530

    Edite image file and readme file

--- src/data/constants.js
+++ src/data/constants.js
@@ -319,7 +319,7 @@ export const education = [
     school: "Academy of Computer Programming And Training - ACPT",
     date: "Sep 2023 - dec 2023",
     grade: "92.3%",
-    desc: "I completed my Web Development and Mobile Application Development both courses at ACPT.",
+    desc: "I have successfully completed web development and mobile application development courses from AT.",
     degree: "AMAD & APWD",
   },
 ];
